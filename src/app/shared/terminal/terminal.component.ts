8c8aeb8a099d078ce7773a6bfad8d9c38b07858c
saucecodee <ashawinta@gmail.com>
2020-10-30 02:58:46 +0100

    feat: copy to clip board for the ternmial

--- src/app/shared/terminal/terminal.component.ts
+++ src/app/shared/terminal/terminal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-terminal',
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terminal.component.scss']
 })
 export class TerminalComponent implements OnInit {
+  @Input() isCopiable: boolean = true
+  @Input() text: string = ""
+  isCoppied = false
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  copyText() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.text;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+
+    this.isCoppied = true
+
+    setTimeout(() => {
+      this.isCoppied = false
+    }, 3000);
   }
 
 }
